ec0d85c188b45d35b5866b2ae8de4a58680ac933
kiristoky1 <151371288+kiristoky1@users.noreply.github.com>
2025-03-07 15:06:57 +0700

    Backend & Model of Google login and email Login

---
+++ app/build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    kotlin("plugin.serialization") version "1.9.0"
+}
+
+android {
+    namespace = "com.chrisp.setaraapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.chrisp.setaraapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.1.2"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:auth-kt")
+    implementation("io.github.jan-tennert.supabase:realtime-kt")
+
+    implementation("io.ktor:ktor-client-android:3.1.1")
+
+    implementation(platform(libs.supabase.bom))
+    implementation(libs.auth.kt)
+
+    implementation(libs.ktor.client.okhttp)
+
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
+
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/com/chrisp/setaraapp/Model/DataAuth/AuthManager.kt
@@ -0,0 +1,116 @@
+package com.chrisp.setaraapp.Model.DataAuth
+
+import android.R.attr.password
+import android.content.Context
+import android.util.Log
+import androidx.credentials.GetCredentialRequest
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.auth.OtpType
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.Google
+import io.github.jan.supabase.auth.providers.builtin.Email
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.security.MessageDigest
+import java.util.UUID
+import androidx.credentials.CredentialManager
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import io.github.jan.supabase.auth.providers.builtin.IDToken
+
+
+sealed interface AuthResponse {
+    data object Success : AuthResponse
+    data class Error(val message: String?) : AuthResponse
+}
+
+class AuthManager(
+    private val context: Context
+) {
+
+    private val supabase = createSupabaseClient(
+        supabaseUrl = "https://kvpupfnfondfwnguhjug.supabase.co",
+        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2cHVwZm5mb25kZnduZ3VoanVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5ODAyMTgsImV4cCI6MjA1NjU1NjIxOH0.ijWBN0onfpzt16PjfIqR6javyLRSkpXukS7yIhAk_k4"
+    ) {
+        install(Auth)
+    }
+
+    fun signUpWithEmail(emailValue: String, passwordValue: String): Flow<AuthResponse> = flow {
+        try {
+            supabase.auth.signInWith(Email){
+                email = emailValue
+                password = passwordValue
+            }
+            emit(AuthResponse.Success)
+        } catch (e: Exception) {
+            emit(AuthResponse.Error(e.localizedMessage))
+        }
+    }
+
+    fun signInWithEmail(emailValue: String, passwordValue: String): Flow<AuthResponse> = flow {
+        try {
+            supabase.auth.signInWith(Email) {
+                email = emailValue
+                password = passwordValue
+            }
+            emit(AuthResponse.Success)
+
+        } catch (e: Exception) {
+            emit(AuthResponse.Error(e.localizedMessage))
+        }
+    }
+
+    fun createNonce(): String {
+        val rawNonce = UUID.randomUUID().toString()
+        val bytes = rawNonce.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val digest = md.digest(bytes)
+
+        return digest.fold("") { str, it ->
+            str + "%02x".format(it)
+        }
+    }
+
+    fun loginGoogleUser(): Flow<AuthResponse> = flow {
+        val hashedNonce = createNonce()
+
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setServerClientId("238120396342-p7a849jcvs8v9hj636c4ln32ofiidd2k.apps.googleusercontent.com")
+            .setNonce(hashedNonce)
+            .setAutoSelectEnabled(false)
+            .setFilterByAuthorizedAccounts(false)
+            .build()
+
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+        val credentialManager = CredentialManager.create(context)
+
+        try {
+            val result = credentialManager.getCredential(
+                context = context,
+                request = request
+            )
+
+            val googleIdTokenCredential = GoogleIdTokenCredential
+                .createFrom(result.credential.data)
+
+            val googleIdToken = googleIdTokenCredential.idToken
+
+            supabase.auth.signInWith(IDToken) {
+                idToken = googleIdToken
+                provider = Google
+            }
+
+            emit(AuthResponse.Success)
+
+        } catch (e: Exception) {
+            Log.e("google", e.localizedMessage)
+            emit(AuthResponse.Error(e.localizedMessage))
+        }
+    }
+
+
+}
